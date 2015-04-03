d9c64eaa554b10e11326d92696f508ca8ef5e235
drmikou <phanthavong.mickael@gmail.com>
2015-04-03 17:07:29 +0200

    Update header.php

--- header.php
+++ header.php
@@ -4,24 +4,80 @@
 	<header>
 
 		<img src="images/Logo.png" class="imageflottante" alt="logo" width="250" heigh="250" />
-
+                        
 		<div id="titre_principal">
-			<h1>Veg A table</h1>
+                    
+			<h1>
+                            <a href="home.php">
+                                Veg A table
+                            </a>
+                        </h1>
 		</div>
 	                
 		<nav>
-			<ul>
-				<li><a href="home.php">Accueil</a></li>
-				<li><a href="trade.php">Echanges</a></li>
-				<!--<li><a href="#">Achat direct</a></li> -->
-				<li><a href="recette.php">Recettes</a></li>
-				<li><a href="#">Fruits & Legumes</a></li>
-				<li><a href="connexion.php">Connexion</a></li>
-				<li><a href="#">Profil</a></li>	
-				<li><a href="#">Forum</a></li>
-				<li><a href="panier.php">Panier</a></li>
-			</ul>
-		</nav>
+                        <div id="conteneur_menu">
+                            <div id="menu">
+                                <dl>
+                                    <dt><a href="home.php">Accueil</a></dt>
+
+                                </dl>
+                                
+                                <!-- Page d'achats -->
+                                <dl>
+                                    <dt><a href="Page d'achats.html">Achat direct</a></dt>
+                                    <dd>
+                                        <ul>
+                                            <li>Offres</li>
+                                            <li>Echanges</li>
+                                        </ul>
+                                    </dd>
+                                </dl>
+                                
+                                <!-- Page de recettes -->         
+                                <dl>
+                                    <dt><a href="Page recette.html">Recettes</a></dt>
+                                        <dd>
+                                            <ul>
+                                                <li>Top des recettes</li>
+                                                <li>Les recettes</li>
+                                            </ul>
+                                        </dd>
+                                </dl>
+                                
+                                <!-- Fruits et légumes -->
+                                <dl>
+                                    <dt><a href="#">Fruits et Légumes</a></dt>
+                                    <dd>
+                                        <ul>
+                                            <li>Ajouter des légumes</li>
+                                        </ul>
+                                    </dd>
+                                </dl>
+                                  
+                                <!-- Profil -->
+                                    <dl>
+                                        <dt><a href="#">Profil</a></dt>
+                                        <dd>
+                                            <ul>
+                                                <li>Mon profil</li>
+                                                <li>Mes messages</li> 
+                                            </ul>
+                                        </dd>
+                                    </dl>
+                                
+                                <!-- Forum -->
+                                    <dl>
+                                        <dt><a href="#">Forum</a></dt>
+                                    </dl>
+                                
+                                 <!-- Connexion -->
+                                <dl>
+                                    <dt><a href="connexion.php">Connexion</a></dt>
+                                </dl>
+                                
+                            </div>
+                        </div>
+                </nav>
 
 	</header>
 
