9065789d8deef96c4924da710932b972048abaca
drmikou <phanthavong.mickael@gmail.com>
2015-04-16 12:00:04 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,21 @@
+<footer>
 
-	<footer>
-
-		<div id="images/reseaux_sociaux">
-
-			<ul>
-				<li><a href="#"><img src="images/rss.png" alt="RSS" /></a></li>
-				<li><a href="#"><img src="images/twitter.png" alt="Twitter" /></a></li>
-				<li><a href="#"><img src="images/facebook.png" alt="Facebook" /></a></li>
-				<li><a href="#"><img src="images/vimeo.png" alt="Vimeo" /></a></li>
-				<li><a href="#"><img src="images/flickr.png" alt="Flickr" /></a></li>
-			</ul>
-			
-			<dt><a href="qui_sommes_nous.php">Qui sommes nous?</a></dt>
-			<dt><a href="faq.php">Foire aux questions</a></dt>
-			<dt><a href="contacter.php">Nous contacter</a></dt>
-			<dt><a href="conditions.php">Conditions générales d'utilisation</a></dt>
-		</div>
-
-	</footer>
+	<div id="images/reseaux_sociaux">
+                    
+                <div id='colonne1'>
+                        <ul>
+                            <li><a href="#"><img src="images/rss.png" alt="RSS" /></a></li>
+                            <li><a href="#"><img src="images/twitter.png" alt="Twitter" /></a></li>
+                            <li><a href="#"><img src="images/facebook.png" alt="Facebook" /></a></li>
+                            <li><a href="#"><img src="images/vimeo.png" alt="Vimeo" /></a></li>
+                            <li><a href="#"><img src="images/flickr.png" alt="Flickr" /></a></li>
+                        </ul>
+                    </div>
+                    
+                    <div id='colonne2'>
+                        <li><a href="qui_sommes_nous.php">Qui sommes nous?</a></li>
+                        <li><a href="faq.php">Foire aux questions</a></li>
+                        <li><a href="contacter.php">Nous contacter</a></li>
+                        <li><a href="conditions.php">Conditions générales d'utilisation</a></li>
+                    </div>
+</footer>
