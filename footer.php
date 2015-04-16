33e3afb51630a9c3228c07675e6c1cc76a4233d1
drmikou <phanthavong.mickael@gmail.com>
2015-04-16 10:46:11 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,7 +10,11 @@
 				<li><a href="#"><img src="images/vimeo.png" alt="Vimeo" /></a></li>
 				<li><a href="#"><img src="images/flickr.png" alt="Flickr" /></a></li>
 			</ul>
-
+			
+			<dt><a href="qui_sommes_nous.php">Qui sommes nous?</a></dt>
+			<dt><a href="faq.php">Foire aux questions</a></dt>
+			<dt><a href="contacter.php">Nous contacter</a></dt>
+			<dt><a href="conditions.php">Conditions générales d'utilisation</a></dt>
 		</div>
 
 	</footer>
