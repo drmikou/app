432263f511c4686306ea8c52c9b57e721ee0116f
drmikou <phanthavong.mickael@gmail.com>
2015-05-05 11:59:55 +0200

    Create messages.php

---
+++ messages.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>vAt accueil</title>
+    </head>
+
+
+    <body>
+
+		<!-- Header -->
+			<?php include("header.php"); ?>
+    	
+		<!-- Corps de l'accueil-->
+			<div id="bloc_page">    	            
+				<section>
+
+					<article>
+						<h1>Mon profil</h1>
+							<p>les infos</p>
+			                    <p></p>
+			                    <p></p>
+					</article>
+
+					<!-- Aside-->
+						<?php include("aside.php"); ?>
+				</section>
+                        </div>
+                
+                    <!-- Footer-->
+			<?php include("footer.php"); ?>
+		
+    </body>
+
+</html>
