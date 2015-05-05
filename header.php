386925d8fd227ce78bf18f35ad7887af9f4d6363
drmikou <phanthavong.mickael@gmail.com>
2015-05-05 11:56:59 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,64 +1,66 @@
-
-	<header>
-
-		<img src="images/Logo.png" class="imageflottante" alt="logo" width="250" heigh="250" />
+<header>
+    <a href="home.php">
+        <img  src="images/Logo.png" class="imageflottante" alt="logo" width="250" heigh="250" />
+    </a>
                         
-		<div id="titre_principal">
-                    
-			<h1>
-                            <a href="home.php">
-                                Veg A table
-                            </a>
-                        </h1>
-		</div>
+    <div id="titre_principal">
+        <h1>
+            <a href="home.php">
+                Veg A table
+            </a>
+        </h1>
+    </div>
 	                
-		<nav>
-                        <div id="conteneur_menu">
-                            <div id="menu">
+    <nav>
+        <div id="conteneur_menu">
+            <div id="menu">
+                <!-- Page d'achats -->
+	            <dl>
+	                <dt><a href="trade.php"> Echanges </a></dt>  
+	            </dl>
                                 
-                                <!-- Page d'achats -->
-	                                <dl>
-	                                    <dt><a href="trade.php"> Echanges </a></dt>  
-	                                </dl>
+                <!-- Page de recettes -->         
+	            <dl>
+	                <dt><a href="recette.php">Recettes</a></dt>
+	                    <dd>
+	                        <ul>
+	                            <li>Top des recettes</li>
+	                        </ul>
+	                    </dd>
+	            </dl>
                                 
-                                <!-- Page de recettes -->         
-	                                <dl>
-	                                    <dt><a href="recette.php">Recettes</a></dt>
-	                                        <dd>
-	                                            <ul>
-	                                                <li>Top des recettes</li>
-	                                                <li>Les recettes</li>
-	                                            </ul>
-	                                        </dd>
-	                                </dl>
-                                
-                                <!-- Fruits et légumes -->
-					<dl>
-	                                    <dt><a href="#">Fruits et Légumes</a></dt>
-	                                </dl>
+                <!-- Fruits et légumes -->
+                    <dl>
+	                <dt><a href="#">Fruits et Légumes</a></dt>
+	            </dl>
                                   
-                                <!-- Profil -->
-					<dl>
-						<dt><a href="#">Profil</a></dt>
-	                                        <dd>
-	                                            <ul>
-	                                                <li>Mon profil</li>
-	                                                <li>Mes messages</li> 
-	                                            </ul>
-	                                        </dd>
-                                	</dl>
+                <!-- Profil -->
+                    <dl>
+			<dt><a href="profil.php">Utilisateur</a></dt>
+	                    <dd>
+	                        <ul>
+	                            <li><a href="messages.php">Mes messages</a></li>
+	                            <li><a href="parametre.php">Paramètres</a></li> 
+	                        </ul>
+	                    </dd>
+                    </dl>
                                 
-                                <!-- Forum -->
-					<dl>
-						<dt><a href="#">Forum</a></dt>
-					</dl>
+                <!-- Forum -->
+                    <dl>
+			<dt><a href="#">Forum</a></dt>
+                    </dl>
                                 
-                                 <!-- Connexion -->
-					<dl>
-	                                    <dt><a href="connexion.php">Connexion</a></dt>
-	                                </dl>
+                <!-- Connexion -->
+                    <dl>
+	                <dt><a href="connexion.php">Connexion</a></dt>
+	            </dl>
+                                 
+                <!-- Connexion -->
+                    <dl>
+	                <dt><a href="panier.php">Panier</a></dt>
+	            </dl>
                                 
-                            </div>
-                        </div>
-                </nav>
-	</header>
+            </div>
+        </div>
+    </nav>
+</header>
