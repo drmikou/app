c6e92fe8ee6b659c018873c3a46c895e5acc73cb
Tiiitou <thi.tan.vincent@gmail.com>
2015-05-05 11:14:47 +0200

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>vAt accueil</title>
+    </head>
+
+    <body>
+        <!-- Header -->
+            <?php include("header.php"); ?>
+
+        <!-- Corps de FAQ-->  
+        <div id="bloc_page">
+        <article>
+             <h3> Foire aux questions </h3>
+        <i>
+            <a href="#comment_creer"><ol> Comment créer une vente de fruits ou légumes ?</ol></a>
+        <ol> Mon vendeur/acheteur n’est pas venu au rendez-vous convenu, que puis-je faire ? </ol>
+        <ol> Je ne suis pas satisfait de mon produit, ai-je un recours possible ? </ol>
+        <ol> Je n’arrive pas à me connecter. </ol>
+        <ol> Comment puis-je connaître l’adresse d’un client/vendeur ? </ol>
+        <ol> J’ai perdu mon mot de passe, comment puis-je le retrouver ? </ol>
+        <ol> Je ne trouve pas le produit souhaité, est-il possible d’élargir mon champ de recherche à une autre géolocalisation ?</ol>
+        <ol> J’ai déménagé, où puis-je changer mon adresse ? </ol>
+        <ol> Comment poster une annonce sur le forum/chat ? </ol>
+        <ol> Mon compte a été piraté et le(s) pirate(s) ont effectué plusieurs achats/ventes à mon insu, que puis-je faire ?</ol>
+        <ol> Mon compte a été banni, puis-je en créer un autre ?</ol>
+        <ol> Je n’arrive pas à vendre dans ma région, puis-je diffuser mon annonce à des zones plus éloignées ?</ol>
+        <ol> Je veux annuler un achat ou une vente, comment puis-je faire ?</ol>
+        <ol> Puis-je avoir plusieurs adresses de livraison ?</ol>
+        </i>
+        </article>
+            
+        <div id="comment_creer">
+            <article> <u>Comment créer une vente de fruits ou légumes ?</u> 
+        
+                <br><br> Pour créer une vente, vous devez posséder un compte GRATUIT sur notre site internet VegATable et rendez-vous sur le lien suivant :
+            </article>
+        </div>
+            <article> <u>Mon vendeur/acheteur n’est pas venu au rendez-vous convenu, que puis-je faire ?
+</u>
+                <br><br> Malheureusement, étant un site à vocation non lucratif, il n’existe pas de recours immédiat pour sanctionner ce dernier. Néanmoins vous pouvez lui donner un retour négatif au niveau de sa note et nous faire un rapport en suivant le lien :
+En cas de plusieurs retours négatifs auprès d’un membre, nous nous réservons le droit de mesures exceptionnelles. 
+            </article>
+<article> <u>Je ne suis pas satisfait de mon produit, ai-je un recours possible ?</u>
+                <br><br> Les notes attribuées à chaque membre est un moyen simple de donner un retour sur sa marchandise. En cas de produits détériorés, vous pouvez nous envoyer une photo et un rapport dans les 48h suivant la vente et nous aviserons en conséquence en cas de récidive ou de vraiment mauvaise qualité. Toutefois, étant un site communautaire, il est préférable que vous arriviez à vous arranger avec le vendeur.
+
+            </article>
+
+<article> <u>Je n’arrive pas à me connecter.</u>
+                <br><br> Vérifiez tout d’abord votre connexion et activez les options pop-up dans votre navigateur. Si toutefois le problème persiste, nous vous invitons à réessayer ultérieurement. 
+</article>
+            
+            <article> <u>Comment puis-je connaître l’adresse d’un client/vendeur ?
+</u>
+                <br><br> L’adresse d’un membre est disponible dans son profil. vous pouvez le consulter en cliquant directement sur le pseudo de ce dernier.</article>
+       
+            <article> <u>J’ai perdu mon mot de passe, comment puis-je le retrouver ?
+</u>
+                <br><br>Si vous avez perdu votre mot de passe, suivez les différentes étapes du lien suivant : </article>
+        
+        </div>
+
+
+        <!-- Footer-->
+            <?php include("footer.php"); ?>
+    </body>
+    
+</html>
