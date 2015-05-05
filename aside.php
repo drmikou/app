f62485bb93875a2fa4015b32ad9b454fb39eb808
drmikou <phanthavong.mickael@gmail.com>
2015-05-05 11:47:03 +0200

    Update aside.php

--- aside.php
+++ aside.php
@@ -1,14 +1,11 @@
-
-	<aside>
-
-		<div id="Nos_Produits">
-			<h1>Nos Produits</h1>
-			<p>
-				<img src="images/banane.png" alt="Photographie" width="100" heigh="100"/>
-				<img src="images/cerise.jpg" alt="Photographie" width="100" heigh="100"/>
-				<img src="images/fraise.jpg" alt="Photographie"width="100" heigh="100" />
-				<img src="images/pomme_terre.jpg" alt="Photographie" width="100" heigh="100"/>
-			</p>
-		</div>
-
-	</aside>
+<aside>
+	<div id="Nos_Produits">
+		<h1>Nos Produits</h1>
+		<p>
+			<img src="images/banane.png" alt="Photographie" width="100" heigh="100"/>
+			<img src="images/cerise.jpg" alt="Photographie" width="100" heigh="100"/>
+			<img src="images/fraise.jpg" alt="Photographie"width="100" heigh="100" />
+			<img src="images/pomme_terre.jpg" alt="Photographie" width="100" heigh="100"/>
+		</p>
+	</div>
+</aside>
