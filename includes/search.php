e475f0d78bb8af20ff888ef523982824e99d09a5
Niccio <nicolas.hugget@isep.fr>
2015-05-06 22:27:15 +0200

    Update search.php

--- includes/search.php
+++ includes/search.php
@@ -12,11 +12,12 @@
 
 <!-- RECHERCHE -->
 <?php
-	if (isset($_GET['q']))	//On vérifie qu'il y a un contenu dans la barre de recherche:
+	header( 'content-type: text/html ; charset=UTF-8 ');
+	if (isset($_GET['barre_recherche']))	//On vérifie qu'il y a un contenu dans la barre de recherche:
 	{
 		$reponse = $db->query('SELECT * FROM recette');		// Initialisation de la variable réponse
-		$q = $_GET['barre_recherche'];						// On récupère tout le contenu de la barre de recherche:
-		$s = explode(" ",$q);								// On divise ce contenu en mots
+		$barre_recherche = $_GET['barre_recherche'];						// On récupère tout le contenu de la barre de recherche:
+		$s = explode(" ",$barre_recherche);								// On divise ce contenu en mots
 		$request = "SELECT * FROM recette";					// On récupère tout le contenu de la table recette:
 
 		$i=0;
@@ -26,7 +27,7 @@
 			{
 				if ($i==0)			 
 				{
-					$request = $request."WHERE ";
+					$request = $request." WHERE ";
 				}
 				else
 				{
@@ -36,42 +37,57 @@
 				$i=$i+1;
 			}
 		}
+		echo $request;
 
 		$reponse = $db->query($request);
 		$presence_reponse = 0;
-		while ($donnees = $reponse->fetch())	// On affiche chaque entrée une à une
-		{	
-			$presence_reponse = 1;
-			$j=0;
-			$c2=$donnees["titre"];
-
-			foreach ($s as $mot)
-			{
-				if(strlen($mot) > 3)
-				{
-					$j++;
-					if($j>4)
-					{
-						$j=1;
+		while ($donnees = $reponse->fetch()){
+			$presence_réponse = 1;
+			?>
+			<article>
+			<?php 
+				$j=0;
+				$c2=$donnees["titre"];
+				foreach ($s as $mot) {
+					if(strlen($mot) > 3){
+						$j++;
+						if($j>4){$j=1;}
+							$c2= str_ireplace( $mot,'<span class="surlign'.$j.'">'.$mot.'</span>', $c2);
+						}
 					}
-					$c2= str_ireplace( $mot,'<span class="surlign'.$j.'">'.$mot.'</span>', $c2);
-				}
-			}
-
-			$i=0;
-			$c=$donnees["contenu"];
-			foreach ($s as $mot)
-			{
-				if(strlen($mot) > 3)
-				{
+			?>
+				<h1> <?php echo $c2; ?></h1>
+			<?php 
+				$i=0;
+				$c=$donnees["contenu"];
+				foreach ($s as $mot) {
+				if(strlen($mot) > 3){
 					$i++;
-					if($i>4)
-					{
-						$i=1;
+					if($i>4){$i=1;}
+						$c= str_ireplace( $mot,'<span class="surlign'.$i.'">'.$mot.'</span>', $c);
 					}
-					$c= str_ireplace( $mot,'<span class="surlign'.$i.'">'.$mot.'</span>', $c);
 				}
-			}								
+			?>
+				<p> <?php echo $c; ?></p>
+			</article>
+											
+			<?php
 		}
+		if($presence_réponse != 1 ){ 
+		?> 
+		<article>
+			<p> <?php echo "Votre recherche n'a pas aboutie! "; ?></p>
+			<p> <?php echo "Veuillez modifiez les paramètres de cette dernière.";?></p>
+		</article>
+		<?php 
+		}
+		$reponse->closeCursor(); // Termine le traitement de la requête
 	}
+	else{
+	?>
+	 <article>
+		<p> le champ de recherche est vide !</p>
+	</article>
+	<?php
+}
 ?>
