0e1212a90fa5dd156f2d5bc63bce64348f36cbc9
Sarpoon <aurelienduval33@yahoo.fr>
2015-05-06 23:55:41 +0200

    Update nous_contacter.php

--- nous_contacter.php
+++ nous_contacter.php
@@ -9,7 +9,11 @@
         <!-- Corps de contacter-->                     
         <div id="bloc_page">
             <article2>
-                <h3> Nous contacter </h3>
+                <h2> Nous contacter </h3>
+		<h3>VegAtable à votre service</h2>
+		<br><p>Par courier : VegAtable service clientèle <br> 10 rue de Vanves <br>92130 ISSY LES MOULINEAUX</p>
+		<br><p>Par téléphone : VegAtable service clientèle <br> 01283748xx</p>
+		<br><p>Par couriel : VegAtable service clientèle <br> sav.online@vegatable.fr</p>
             </article2>
         </div>       
             
