042daa9a7dc7c1062d8e6ac1cef69c253bb7ca85
drmikou <phanthavong.mickael@gmail.com>
2015-05-26 11:54:20 +0200

    Create ajouter.php

---
+++ ajouter.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    $titre = "Echanges";
+    include("includes/start.php");
+?>
+
+    <body>
+        <!-- Aside de l'échange-->
+        <?php include("includes/aside_echange.php"); ?>
+        
+	<!-- Corps de l'échange-->
+        <?php include("includes/echange_proposer.php"); ?>
+            
+        <!-- Footer-->
+        <?php include("includes/footer.php"); ?>
+    </body>
+
+</html>
