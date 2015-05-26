c4b7c0e533e6c9b4009be3fa31f9ae911348cd9c
drmikou <phanthavong.mickael@gmail.com>
2015-05-26 11:51:20 +0200

    Create echange_proposer.php

---
+++ echange_proposer.php
@@ -0,0 +1,23 @@
+<form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES); ?>" method="post">
+    
+    <fieldset>
+        <legend>Proposer un fruit / légume</legend>
+            <p>
+                <label for="login">Mon fruit / légume:</label> <!-- it provides a usability improvement for mouse users, because if the user clicks on the text within the <label> element, it toggles the control. -->
+                <input type="text" name="add" id="add" value=""/>
+            </p>
+
+            <p>
+                <label for="nbr">Quantité :</label> 
+                <input type="number" name="nbr" id="kg" value="" min="0" />
+                kg
+                <input type="number" name="nbr" id="g" value="" min="0"/>
+                g
+            </p>
+            
+            <p>
+                <input type="submit" name="submit" value="Proposer un fruit" />
+            </p>
+    </fieldset>
+    
+</form>
