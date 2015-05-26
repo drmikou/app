da4a79dfb289f4348e30307cb6d1baeeb4d6f8b6
drmikou <phanthavong.mickael@gmail.com>
2015-05-26 11:52:45 +0200

    Create aside_echange.php

---
+++ includes/aside_echange.php
@@ -0,0 +1,18 @@
+<!-- Côté de l'échange-->   
+    <div class="cote">
+        <section>
+            <div class="box">                          
+                <ul class="titre_list"> Achats / ventes </ul>
+                    <li>    <a href="ajouter.php"> Ajouter un fruit / légume </a> </li>
+                    <li>    <a href="choisir.php"> Choisir un fruit / légume </a> </li>
+            </div>
+        </section>
+                        
+        <section>
+            <div class="box">                          
+                <ul class="titre_list"> Mes échanges </ul>
+                    <li>    <a href=""> Mes échanges en cours </a> </li>
+                    <li> Mes offres </li>
+            </div>
+        </section>            
+    </div>
