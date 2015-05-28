8084eae26fd33b461b52187a47b44110fd163f1d
drmikou <phanthavong.mickael@gmail.com>
2015-05-28 10:31:50 +0200

    Delete news.php

--- includes/news.php
+++
@@ -1,6 +0,0 @@
-<div id="banniere_image">
-    <div id="banniere_description">
-	Actualités du site
-        <a href="actualités.php" class="bouton_rouge">Toutes les actualités <img src="images/ico_liensexterne.png" alt="" /></a>
-    </div>
-</div>
