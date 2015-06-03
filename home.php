adec7af5948f3158dc277549ca2724d38e7af0e8
Sarpoon <aurelienduval33@yahoo.fr>
2015-06-03 15:26:08 +0200

    Update home.php

--- home.php
+++ home.php
@@ -52,51 +52,7 @@
 							</div>
 						</section>
 				</div>
-				<div id="bloc_chat" class="centrage">
-				<article>
-					<?php // VISITEUR
-						if($id==0)
-					{
-				    ?>
-					<div id="chat">					
-						<?php
-						$allmsg = $db->query('SELECT * FROM chat ');
-							while($msg = $allmsg->fetch())
-							{
-						?>
-						<b><?php echo $msg['chat_pseudo']; ?> :</b> <?php echo date($msg['chat_date']) ; ?>   	<?php echo $msg['chat_message']; ?><br>
-						<?php
-							}
-						?>
-						</div>
-					<?php
-					}
-						else
-					{
-					?>
-					<div id="chat">					
-						<?php
-						$allmsg = $db->query('SELECT * FROM chat ');
-							while($msg = $allmsg->fetch())
-							{
-						?>
-						<b><?php echo $msg['chat_pseudo']; ?> :</b> <?php echo date($msg['chat_date']) ; ?>   	<?php echo $msg['chat_message']; ?><br>
-						<?php
-							}
-						?>
-						</div>
-					<div id="saisie_chat" >
-							<b><?php if(isset($pseudo)){ echo $pseudo ;}?></b>
-						<form method="post" action=""> 
-							<textarea type="text" name="message" placeholder"MESSAGE"></textarea><br>
-							<input type="submit" value="Envoyer" />					
-						</form>
-					</div>
-					<?php
-					}
-					?> 
-				</article>
-			</div>
+			<?php include("includes/chat.php"); ?>
 		</div>
 	</body>
 			<!-- Footer-->
