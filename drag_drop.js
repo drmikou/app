0cefc95f04681d4cfac5de49377bd88f0d9bd366
drmikou <phanthavong.mickael@gmail.com>
2015-06-03 15:19:59 +0200

    ee
    eee

--- drag_drop.js
+++ drag_drop.js
@@ -1,8 +1,63 @@
-var dragImg = new Image(); // Il est conseillé de précharger l'image, sinon elle risque de ne pas s'afficher pendant le déplacement
-    dragImg.src = 'drag_img.png';
+(function()
+{
 
-document.querySelector('*[draggable="true"]').addEventListener('dragstart', function(e) {
+    var dndHandler =
+    {	// Fonction du drag & drop
+      
+        applyDragEvents: function(element)		// -------- Rend déplaçable element --------
+        {
+        	element.draggable = true;
 
-    e.dataTransfer.setDragImage(dragImg, 40, 40); // Une position de 40x40 pixels centrera l'image (de 80x80 pixels) sous le curseur
+        	var dndHandler = this;								// Cette variable est nécessaire pour que l'événement « dragstart » accède facilement au namespace « dndHandler »
 
-}, false);
+        	element.addEventListener('dragstart', function(e)
+        	{
+            	dndHandler.draggedElement = e.target; 				// On sauvegarde l'élément en cours de déplacement	/ The target event property returns the element that triggered the event.
+            	e.dataTransfer.setData('text/plain', '');			// Nécessaire pour Firefox
+        	}, false);
+
+
+        applyDropEvents: function(dropper)		// -------- Gère les évènements au niveau des zones de drop --------
+        {
+        	dropper.addEventListener('dragover', function(e)	// Lorsqu'on passe sur la zone
+        	{
+            	e.preventDefault();									// On autorise le drop d'éléments  / preventdefault() :the default action of the event will not be triggered.
+            	this.className = 'dropper drop_hover';				// Et on applique le style adéquat à notre zone de drop quand un élément la survole
+        	}, false);
+        
+        	dropper.addEventListener('dragleave', function()	// Lorsqu'on quitte la zone
+        	{
+            	this.className = 'dropper';							// On revient au style de base lorsque l'élément quitte la zone de drop
+        	});
+
+        	dropper.addEventListener('drop', function(e)		// Lorsqu'on lache l'élément
+        	{
+    			var target = e.target,
+        		draggedElement = dndHandler.draggedElement,			// 1) Récupération de l'élément concerné
+        		clonedElement = draggedElement.cloneNode(true);		// 2) Création d'un clone de l'élément 			/ cloneNode(): Create a deep copy of the set of matched elements.
+    			target.className = 'dropper';						// 3) Application du style par défaut sur l'élément
+    			clonedElement = target.appendChild(clonedElement);	// 4) Ajout de l'élément cloné à la zone de drop actuelle
+    			dndHandler.applyDragEvents(clonedElement); 			// 5) Nouvelle application des événements qui ont été perdus lors du cloneNode() / cloneNode() ne conserve pas les événements.
+    			draggedElement.parentNode.removeChild(draggedElement); // 6) Suppression de l'élément d'origine
+    		});
+    	}	
+
+    };
+
+    // Utilise les fonctions
+
+    var elements = document.querySelectorAll('.draggable'),
+     elementsLen = elements.length;
+
+    for (var i = 0 ; i < elementsLen ; i++) {
+        dndHandler.applyDragEvents(elements[i]); // Application des paramètres nécessaires aux éléments déplaçables
+    }
+
+    var droppers = document.querySelectorAll('.dropper'),
+        droppersLen = droppers.length;
+
+    for (var i = 0 ; i < droppersLen ; i++) {
+        dndHandler.applyDropEvents(droppers[i]); // Application des événements nécessaires aux zones de drop
+    }
+
+})();
--- includes/echange_choisir.php
+++ includes/echange_choisir.php
@@ -16,7 +16,7 @@
             </p>
 
         <div class="dropper">
-            <div class="draggable">#1</div>
+            <div class="draggable">pomme</div>
             <div class="draggable">#2</div>
         </div>
 
@@ -25,10 +25,85 @@
             <div class="draggable">#4</div>
         </div>
 
+        <script>
+            (function() {
+        
+                var dndHandler =
+                {   // Fonction du drag & drop
+                    draggedElement: null,                 
 
-            <p>
-                <input type="submit" name="submit" value="Ajouter au panier" />
-            </p>
+                    applyDragEvents: function(element)      // -------- Rend déplaçable element --------
+                    {
+                        element.draggable = true;
+                        var dndHandler = this;              // Cette variable est nécessaire pour que l'événement "dragstart" ci-dessous accède facilement au namespace "dndHandler"
+                
+                        element.addEventListener('dragstart', function(e)
+                        {
+                            dndHandler.draggedElement = e.target; // On sauvegarde l'élément en cours de déplacement
+                            e.dataTransfer.setData('text/plain', 'document.write(this.id);'); // Nécessaire pour Firefox
+                        }, false);
+                
+                    },
+     
+                    applyDropEvents: function(dropper)
+                    {
+                        dropper.addEventListener('dragover', function(e)
+                        {
+                            e.preventDefault();                     // On autorise le drop d'éléments
+                            this.className = 'dropper drop_hover';  // Et on applique le design adéquat à notre zone de drop quand un élément la survole
+                        }, false);
+                
+                        dropper.addEventListener('dragleave', function()
+                        {
+                            this.className = 'dropper'; // On revient au design de base lorsque l'élément quitte la zone de drop
+                        });
+                
+                        var dndHandler = this; // Cette variable est nécessaire pour que l'événement "drop" ci-dessous accède facilement au namespace "dndHandler"
+
+                        dropper.addEventListener('drop', function(e)
+                        {
+                            var target = e.target,
+                            draggedElement = dndHandler.draggedElement,         // 1) Récupération de l'élément concerné
+                            clonedElement = draggedElement.cloneNode(true);     // 2) On créé immédiatement le clone de cet élément
+                    
+                            while(target.className.indexOf('dropper') == -1)    // Cette boucle permet de remonter jusqu'à la zone de drop parente
+                            { 
+                                target = target.parentNode;
+                            }
+
+                            target.className = 'dropper';                       // 3) Application du design par défaut
+                    
+                            clonedElement = target.appendChild(clonedElement);  // 4) Ajout de l'élément cloné à la zone de drop actuelle
+                            dndHandler.applyDragEvents(clonedElement);          // 5) Nouvelle application des événements qui ont été perdus lors du cloneNode()
+                    
+                            draggedElement.parentNode.removeChild(draggedElement); // 6) Suppression de l'élément d'origine
+                            alert(e.dataTransfer.getData('text/plain')); // Affiche le contenu du type MIME « text/plain »
+                        });
+                    }
+                };
+        
+                var elements = document.querySelectorAll('.draggable'),
+                elementsLen = elements.length;
+        
+                for(var i = 0 ; i < elementsLen ; i++)
+                {
+                    dndHandler.applyDragEvents(elements[i]); // Application des paramètres nécessaires aux élément déplaçables
+                }
+        
+                var droppers = document.querySelectorAll('.dropper'),
+                droppersLen = droppers.length;
+        
+                for(var i = 0 ; i < droppersLen ; i++)
+                {
+                    dndHandler.applyDropEvents(droppers[i]); // Application des événements nécessaires aux zones de drop
+                }
+
+            })();
+        </script>
+        
+        <p>
+            <input type="submit" name="submit" value="Ajouter au panier" />
+        </p>
 
     </fieldset>
     
