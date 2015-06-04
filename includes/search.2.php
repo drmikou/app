c0de5df69df05bb5236dd2782b92ad46e33c3528
Niccio <nicolas.hugget@isep.fr>
2015-06-04 10:10:25 +0200

    Create search.2.php

---
+++ includes/search.2.php
@@ -0,0 +1,55 @@
+<!-- CONNEXION A LA DB -->
+	<?php
+	header( 'content-type: text/html ; charset=UTF-8 ');
+		include("includes/database_connection.php");
+	?>
+
+<form method="get" action="fruits_et_legumes.php">
+      Recherche:
+      <input type="text" name="barre_recherche"/>
+      <input type="submit" value="Recherche!"/>
+     </form> 
+<!-- RECHERCHE -->
+<?php
+	if (isset($_GET['barre_recherche']))	//On vérifie qu'il y a un contenu dans la barre de recherche:
+	{
+		$reponse = $db->query('SELECT * FROM fruit_legumes');		// Initialisation de la variable réponse
+		$barre_recherche = $_GET['barre_recherche'];						// On récupère tout le contenu de la barre de recherche:
+		$s = explode(" ",$barre_recherche);								// On divise ce contenu en mots
+		$request = "SELECT * FROM fruit_legumes";					// On récupère tout le contenu de la table recette:
+		$i=0;
+		foreach($s as $mot)		// Parcours $s, incrémente $mot à chaque mot rencontré 
+		{
+			if(strlen($mot) > 3)	// Si la taille de la chaine est au dessus de 3 / strlen: calcule la taille d'une chaîne 
+			{
+				if ($i==0)			 
+				{
+					$request = $request." WHERE ";
+				}
+				else
+				{
+					$request = $request."OR ";
+				}
+				$request = $request."fruit_Legumes_nom LIKE '%$mot%' ";
+				$i=$i+1;
+			}
+		}
+		$request=$request." ORDER BY fruit_Legumes_nom ";
+		echo $request;
+		$reponse = $db->query($request);
+		while ($donnees = $reponse->fetch()){
+?>
+<?php 
+				$c2=$donnees["fruit_legumes_nom"];
+				foreach ($s as $mot) {
+
+					echo'<article> <h1>'.$c2.':</h1> <h1> <img src="images/'.$c2.'.jpg" alt="Image flottante2" class="imageflottante"width="100" heigh="100" /> </h1>'; 
+					echo'<h2> Que designe le mot "'.$c2.'" ?</h2> <p>'.$donnees["fruit_legumes_description"].'</p> </article>';
+				}
+		}
+	$reponse->closeCursor(); // Termine le traitement de la requête
+	}
+	else{
+		echo'<article> <p>Votre recherche n\'a pas aboutie.</p> <p> Veuillez remplir le champ de la barre de recherche</p></article>';
+	}
+?>
