59f646c9fb65327e3d13827964ac7e4193c2aa35
Niccio <nicolas.hugget@isep.fr>
2015-06-05 15:34:43 +0200

    Update fruits_et_legumes.php

--- fruits_et_legumes.php
+++ fruits_et_legumes.php
@@ -9,7 +9,32 @@
             <!-- Corps de recettes-->
             <section>
                <?php include("includes/search.2.php");?>
+            
+            <marquee id="id1" height="110" width="1000" scrollamount="2" loop="0">
 
+                    <span onmouseover="getElementById('id1').stop();" onmouseout="getElementById('id1').start();">
+
+                        <?php 
+
+                            $request='SELECT fruit_legumes_nom FROM fruit_legumes ORDER BY fruit_legumes_nom';
+                        
+                            $reponse = $db->query($request);
+
+                            if(isset($reponse)){
+
+                             while ($donnees = $reponse->fetch()){
+
+                                echo' <a href="fruits_et_legumes.php?barre_recherche='.$donnees["fruit_legumes_nom"].'"><img src="images/fruits_legumes/'.$donnees["fruit_legumes_nom"].'.jpg" title="'.$donnees["fruit_legumes_nom"].'"alt="Image flottante2" class="imageflottante"width="100" heigh="100" /></a> ';
+                             
+                             }
+
+                            }
+
+                            $reponse->closeCursor(); // Termine le traitement de la requête */
+                        ?>
+
+                    </span>
+                </marquee>
             </section>
         </div>
 
