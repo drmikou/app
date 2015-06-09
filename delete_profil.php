cc218370905168bd6b3bc78ada9f188c5580c18f
drmikou <phanthavong.mickael@gmail.com>
2015-06-09 17:43:19 +0200

    dede
    de

--- delete_profil.php
+++ delete_profil.php
@@ -37,9 +37,7 @@
 		{
 			
 			$mdp_actuel= $_POST['mdp_actuel'];
-			
-
-
+		
 			$query = $db->prepare('SELECT user_password FROM user WHERE user_login= :login'); //PDO::prepare — Prépare une requête à l'exécution et retourne un objet
 			$query->execute(array('login' => $pseudo)); // Exécute la préparation
 			$data = $query->fetch();
